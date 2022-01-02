0831fc35fefb073a362f62cca9f6c32a92283a27
ryanguorocket <91096317+ryanguorocket@users.noreply.github.com>
2022-01-01 20:51:12 -0500

    Create P3.cpp

---
+++ DMOPC/2021/Contest 4/P3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout<<0<<"\n";
+    cout<<10001<<"\n";
+    cout<<10101<<"\n";
+    cout<<10201<<"\n";
+    cout<<10301<<"\n";
+    cout<<11011<<"\n";
+    cout<<11111<<"\n";
+    cout<<11211<<"\n";
+    cout<<12021<<"\n";
+    cout<<13031<<"\n";
+}
+//what?
