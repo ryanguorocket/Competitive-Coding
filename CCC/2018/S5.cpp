ba3fe7ca290e3cc4279d6090e0feb2b2fe59dbdb
ryanguorocket <91096317+ryanguorocket@users.noreply.github.com>
2023-02-18 12:45:35 -0500

    Update S5.cpp

--- CCC/2018/S5.cpp
+++ CCC/2018/S5.cpp
@@ -1,56 +1,58 @@
-#include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
+
 typedef long long ll;
-typedef tuple<ll,int,int>tt;
-const int MAX=1e5;
-int n,m,p,q,a,b,parent[MAX][2],si[MAX][2];
-ll total,c;
-vector<tt>edge;
-int findParent(int be,int t){
-    if(parent[be][t]==be)return be;
-    return parent[be][t]=findParent(parent[be][t],t);
+typedef tuple<ll, int, int> tt;
+
+const int MAX = 1e5;
+int n, m, p, q, a, b, parent[MAX][2], si[MAX][2];
+ll total, c;
+vector<tt> edge;
+int findParent(int be, int t) {
+    if(parent[be][t] == be) return be;
+    return parent[be][t] = findParent(parent[be][t], t);
 }
-bool connected(int be,int en,int t){
-    return findParent(be,t)==findParent(en,t);
+bool connected(int be, int en, int t) {
+    return findParent(be, t) == findParent(en, t);
 }
-void connect(int be,int en,int t){
-    if(si[findParent(be,t)][t]>si[findParent(en,t)][t])swap(be,en);
-    parent[findParent(be,t)][t]=findParent(en,t);
-    si[findParent(en,t)][t]+=si[findParent(be,t)][t];
+void connect(int be, int en, int t) {
+    if(si[findParent(be, t)][t] > si[findParent(en, t)][t]) swap(be, en);
+    parent[findParent(be, t)][t] = findParent(en, t);
+    si[findParent(en, t)][t] += si[findParent(be, t)][t];
 }
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin>>n>>m>>p>>q;
-    memset(si,0,sizeof(si));
-    for(int i=0;i<max(n,m);i++){
-        parent[i][0]=i;
-        parent[i][1]=i;
+    cin >> n >> m >> p >> q;
+    memset(si, 0, sizeof(si));
+    for(int i = 0; i < max(n, m); i++) {
+        parent[i][0] = i;
+        parent[i][1] = i;
     }
-    total=0;
-    for(int i=0;i<p;i++){
-        cin>>a>>b>>c;
-        edge.push_back(make_tuple(10*c,a-1,b-1));
-        total+=c*n;
+    total = 0;
+    for(int i = 0; i < p; i++) {
+        cin >> a >> b >> c;
+        edge.push_back(make_tuple(10 * c, a - 1, b - 1));
+        total += c * n;
     }
-    for(int i=0;i<q;i++){
-        cin>>a>>b>>c;
-        edge.push_back(make_tuple(10*c+1,a-1,b-1));
-        total+=c*m;
+    for(int i = 0; i < q; i++) {
+        cin >> a >> b >> c;
+        edge.push_back(make_tuple(10 * c + 1, a - 1, b - 1));
+        total += c * m;
     }
-    sort(edge.begin(),edge.end());
-    for(tt i:edge){
-        if(connected(get<1>(i),get<2>(i),get<0>(i)%10))continue;
-        connect(get<1>(i),get<2>(i),get<0>(i)%10);
-        if(get<0>(i)%10){
-            total-=m*(get<0>(i)/10);
+    sort(edge.begin(), edge.end());
+    for(tt i: edge) {
+        if(connected(get<1> (i), get<2> (i), get<0> (i) % 10)) continue;
+        connect(get<1> (i), get<2> (i), get<0> (i) % 10);
+        if(get<0> (i) % 10) {
+            total -= m * (get<0> (i) / 10);
             n--;
         }
-        else{
-            total-=n*(get<0>(i)/10);
+        else {
+            total -= n * (get<0> (i) / 10);
             m--;
         }
     }
-    cout<<total<<"\n";
+    cout << total << "\n";
 }
